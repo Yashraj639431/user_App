f0f50220ca6eef52a90b9d040748e7d4621a8179
Yash <yashrajoriya321@gmail.com>
2023-02-01 19:53:46 +0530

    updates

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -14,7 +14,7 @@ const Contact = () => {
       <Container class1="contact-wrapper home-wrapper-2 py-5">
         <div className="row">
           <div className="col-12">
-            <iframe
+            <iFrame
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3548.564901716533!2d77.4937656148293!3d27.201408654150768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3973a3148ecd950d%3A0xedc961c0661b7a69!2sKali%20Baghichi%20Rd%2C%20Jawahar%20Nagar%2C%20Bharatpur%2C%20Rajasthan%20321001!5e0!3m2!1sen!2sin!4v1673605725476!5m2!1sen!2sin"
               width="600"
               height="450"
@@ -22,7 +22,7 @@ const Contact = () => {
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
+            ></iFrame>
           </div>
           <div className="col-12 mt-5">
             <div className="contact-liner-wrapper d-flex justify-content-between">
