42faa6087111aac329f93d26f8816022c9051898
Yash <yashrajoriya321@gmail.com>
2023-12-19 23:34:57 +0530

    contact

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -5,8 +5,42 @@ import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
 import { BiPhoneCall } from "react-icons/bi";
 import { BsInfoCircle } from "react-icons/bs";
 import Container from "./Container";
+import * as yup from "yup";
+import { useFormik } from "formik";
+import { useDispatch } from "react-redux";
+import { createQuery } from "../features/contact/contactSlice";
+
+const contactSchema = yup.object({
+  name: yup.string().required("Name is Required"),
+  email: yup
+    .string()
+    .nullable()
+    .email("Email should be valid.")
+    .required("Email is Required"),
+  mobile: yup
+    .string()
+    .default("")
+    .nullable()
+    .required("Mobile Number is Required."),
+  comment: yup.string().default("").nullable().required("Comment is Required."),
+});
 
 const Contact = () => {
+  const dispatch = useDispatch();
+
+  const formik = useFormik({
+    enableReinitialize: true,
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      comment: "",
+    },
+    validationSchema: contactSchema,
+    onSubmit: (values) => {
+      dispatch(createQuery(values));
+    },
+  });
   return (
     <>
       <Meta title={"Contact Us"}></Meta>
@@ -14,7 +48,7 @@ const Contact = () => {
       <Container class1="contact-wrapper home-wrapper-2 py-5">
         <div className="row">
           <div className="col-12">
-            <iFrame
+            <iframe
               src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3548.564901716533!2d77.4937656148293!3d27.201408654150768!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3973a3148ecd950d%3A0xedc961c0661b7a69!2sKali%20Baghichi%20Rd%2C%20Jawahar%20Nagar%2C%20Bharatpur%2C%20Rajasthan%20321001!5e0!3m2!1sen!2sin!4v1673605725476!5m2!1sen!2sin"
               width="600"
               height="450"
@@ -22,46 +56,79 @@ const Contact = () => {
               allowFullScreen=""
               loading="lazy"
               referrerPolicy="no-referrer-when-downgrade"
-            ></iFrame>
+            ></iframe>
           </div>
           <div className="col-12 mt-5">
             <div className="contact-liner-wrapper d-flex justify-content-between">
               <div>
                 <h3 className="contact-title mb-4">Contact</h3>
-                <form action="" className="d-flex flex-column gap-15">
+                <form
+                  action=""
+                  className="d-flex flex-column gap-15"
+                  onSubmit={formik.handleSubmit}
+                >
                   <div>
                     <input
                       type="text"
                       className="form-control"
                       placeholder="Name"
+                      name="name"
+                      onChange={formik.handleChange("name")}
+                      onBlur={formik.handleBlur("name")}
+                      value={formik.values.name}
                     />
+                    <div className="error">
+                      {formik.touched.name && formik.errors.name}
+                    </div>
                   </div>
                   <div>
                     <input
                       type="email"
                       className="form-control"
                       placeholder="Email"
+                      name="email"
+                      onChange={formik.handleChange("email")}
+                      onBlur={formik.handleBlur("email")}
+                      value={formik.values.email}
                     />
+                    <div className="error">
+                      {formik.touched.email && formik.errors.email}
+                    </div>
                   </div>
                   <div>
                     <input
                       type="tel"
                       className="form-control"
                       placeholder="Mobile Number"
+                      name="mobile"
+                      onChange={formik.handleChange("mobile")}
+                      onBlur={formik.handleBlur("mobile")}
+                      value={formik.values.mobile}
                     />
+                    <div className="error">
+                      {formik.touched.mobile && formik.errors.mobile}
+                    </div>
                   </div>
                   <div>
                     <textarea
-                      name=""
                       id=""
                       className="w-100 form-control"
                       cols="30"
                       rows="4"
                       placeholder="Comments"
+                      name="comment"
+                      onChange={formik.handleChange("comment")}
+                      onBlur={formik.handleBlur("comment")}
+                      value={formik.values.comment}
                     ></textarea>
+                    <div className="error">
+                      {formik.touched.comment && formik.errors.comment}
+                    </div>
                   </div>
                   <div>
-                    <button className="button border-0">Submit</button>
+                    <button type="submit" className="button border-0">
+                      Submit
+                    </button>
                   </div>
                 </form>
               </div>
